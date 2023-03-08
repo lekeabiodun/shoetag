3fb74a13c07ce38ae8a4dd8979cc98b543a8bb5e
Broda-smith12 <125408062+Smith-Olatunde@users.noreply.github.com>
2023-03-08 12:01:25 +0100

    update about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,10 +3,13 @@
 @section('content')
 <div class="header">
     <h1 class="about-head">ABOUT</h1>
-    <div class="image-section">
-        <img src="{{ url('img/shoe.png') }}" alt="">
-        <img src="{{ url('img/shoe-3.png') }}" alt="">
-        <img src="{{ url('img/shoe-2.png') }}" alt="">
+    <div class="image-box">
+        <div class="image-section row mt-4">
+            <img src="{{ url('img/shoe.png') }}" alt="" class="col-3">
+            <img src="{{ url('img/shoe-3.png') }}" alt="" class="col-3">
+            <img src="{{ url('img/shoe-2.png') }}" alt="" class="col-3">
+        </div>
+
     </div>
 </div>
 @endsection
