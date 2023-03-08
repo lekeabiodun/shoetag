622140d23ef54f387cab4744bfab54a0a37fd0bf
Broda-smith12 <125408062+Smith-Olatunde@users.noreply.github.com>
2023-03-08 11:35:56 +0100

    add about

---
+++ resources/views/about.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="header">
+    <h1 class="about-head">ABOUT</h1>
+    <div class="image-section">
+        <img src="{{ url('img/shoe.png') }}" alt="">
+        <img src="{{ url('img/shoe-3.png') }}" alt="">
+        <img src="{{ url('img/shoe-2.png') }}" alt="">
+    </div>
+</div>
+@endsection
