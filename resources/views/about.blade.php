303b991552597ffcfe7de8b4705d06827e0f391c
Broda-smith12 <125408062+Smith-Olatunde@users.noreply.github.com>
2023-03-08 12:49:01 +0100

    added arrow

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="header">
-    <h1 class="header-head">ABOUT</h1>
-    <div class="image-box">
-        <div class="image-section row mt-4">
-            <img src="{{ url('img/shoe.png') }}" alt="" class="col-3">
-            <img src="{{ url('img/shoe-3.png') }}" alt="" class="col-3">
-            <img src="{{ url('img/shoe-2.png') }}" alt="" class="col-3">
+<div class="head">
+    <a href="{{ asset('/') }}">
+
+        <img src="{{ url('img/arrow.png') }}" alt="" class="arrow" style="width: 100px;">
+    </a>
+    <div class="header">
+        <h1 class="header-head">ABOUT</h1>
+        <div class="image-box">
+            <div class="image-section row mt-4">
+                <img src="{{ url('img/shoe.png') }}" alt="" class="col-3">
+                <img src="{{ url('img/shoe-3.png') }}" alt="" class="col-3">
+                <img src="{{ url('img/shoe-2.png') }}" alt="" class="col-3">
+            </div>
         </div>
+        <h5 class="about-head mt-4">THANK YOU</h5>
     </div>
-    <h5 class="about-head mt-4">THANK YOU</h5>
 
 </div>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.app')
 
 @section('content')       
-        <div class="header">
+        <div class="head header">
         <h1 class="header-head">HELLO WORLD</h1>
-        <img src="{{ url('img/shoe.png') }}" alt="">
+        <img src="{{ url('img/shoe.png') }}" alt="shoe-home-page">
        <a href="{{ asset('about') }}">
            <button class="btn btn-primary mt-3">ABOUT</button>
        </a>
