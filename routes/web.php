84f1104c322227db7b39470fc56a35d7e85f69df
Broda-smith12 <125408062+Smith-Olatunde@users.noreply.github.com>
2023-03-08 13:45:01 +0100

    refactor the controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,11 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [HomeController::class, 'home'] 
+);
 
-Route::get('about', function () {
-    return view('about');
-});
+Route::get('about', [HomeController::class, 'about'] 
+);
+
+
+// Route::get('about', function () {
+//     return view('about');
+// });
 
